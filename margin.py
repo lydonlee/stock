66555a245659d234b78bb8948fcdea66d5335368
lydonlee <lydonlee@gmail.com>
2019-03-10 11:16:01 +0800

    Add files via upload

--- margin.py
+++ margin.py
@@ -2,11 +2,12 @@ import tushare as ts
 from sqlalchemy import create_engine
 import matplotlib.pyplot as plt
 import numpy as np
-
+import pandas as pd
 class storedata(object):
     def __init__(self):
         ts.set_token('1360474e70eee70c9c1b9740d684a8800e89903641c6ffb82ac549da')
         self.pro = ts.pro_api()
+        self.mysqlrecord = '/Users/liligong/stock/mysqlrecord.csv'
 
     def tradedays(self,start_date1='20180101', end_date1='20181231'):
 
@@ -30,6 +31,28 @@ class storedata(object):
 
         return rqye_list,close_list,tradedays_list
 
+    def datanotinmysql(self,db = 'margin_detail',wstart = '20180101',wend='20180201'):
+        start = wstart
+        end = wend
+
+        df = pd.read_csv(self.mysqlrecord)
+        for row in df.iterrows():
+            if (row['db'] == db) :
+                if start > row['start'] and start < row['end'] and end < row['end']:
+                    start = '0'
+                    end = '0'
+                    return start,end
+                elif start > row['start'] and start < row['end'] and end > row['end']:
+                    start = row['end']
+                    continue
+                elif start < row['start'] and end > row['start'] and end < row['end']:
+                    end = row['start']
+
+        return start , end
+
+    def savemysqlrecorde(self,db = 'margin_detail',wstart = '20180101',wend='20180201'):
+        df = pd.DataFrame([[db,wstart,wend]])
+        df.to_csv(self.mysqlrecord,mode='a',header = False)
 
     def test(self):
         f = df.iloc[0:, [1, 2]]
@@ -39,15 +62,21 @@ class storedata(object):
 
 if __name__ == '__main__':
     sd = storedata()
-    rqye_list,close_list,td_list = sd.rqye(start = '20180101',end = '20180301')
-    tradedays_list = list(map(int,td_list))
+    s = '20180101'
+    e = '20180301'
+
+    rqye_list,close_list,td_list = sd.rqye(start = s,end = e)
+    'tradedays_list = list(map(int,td_list))'
+
+    day= np.arange(0,len(td_list))
     x = np.array(rqye_list,dtype=np.float)
     print(x/100000*5)
     print(close_list)
-    plt.plot(tradedays_list, x/100000*5, tradedays_list, close_list)
-
-    plt.xlabel('time')
+    plt.subplot(211)
+    'plt.axis([0,70,5,40])'
+    plt.plot(day, x/100000*5, day, close_list)
+    plt.xlabel('start:'+s+'end:'+e)
     plt.ylabel('rqye and close')
-    plt.grid(True)
+    plt.grid(axis="y")
 
-    plt.show()
+    print(np.corrcoef(x/100000*5, close_list))
