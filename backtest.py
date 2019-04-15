fec2e2076b99b0c822616c1d01bb71d5cc9d9978
liligong <lydonlee@gmail.com>
2019-04-15 08:00:26 +0800

    add

--- backtest.py
+++ backtest.py
@@ -23,32 +23,33 @@ class Backtest(object):
         self.doit(pdf=dfbuysell,pdate=pdate)
 
     def logic(self,pdf):
-        return pdf
+        df =pdf.copy(deep=True)
+        self._assetalloc(df)
+        self._stocktiming(df)
+        self._stockselection(df)
+        return 
 
     def doit(self,pdf,pdate):
         for i,row in pdf.iterrows():
-            if row['buysell'] == 1: # 1:buy ,-1 sell
-                self.account.buy(row['ts_code'],pdate,row['price'],row['quantity'])
-            else:
-                self.account.sell(row['ts_code'],pdate,row['price'],row['quantity'])
+            self.account.buysell(row,pdate)
+
+    def _stockselection(self,df):
 
 class Account(object):
     def __init__(self,pwhichAccount):
         cfg = config.configs.Backtest
         self.account_csv = cfg.account_csv
         if pwhichAccount == 1:#如果是账户1，从csv读取
-            self.account = pd.read_csv(self.account_csv,index_col = 0)
+            self.journalaccount  = pd.read_csv(self.account_csv,index_col = 0)
             self.buypower = self.account.iloc[-1]['buypower']
         else:
-            acct = {'ts_code':[],'date':[],'price':[],'quantity':[],'buysell':[],'buypower':[],}
-            self.account = pd.DataFrame(acct)
+            acct = {'ts_code':[],'date':[],'price':[],'Shares':[],'buysell':[],'buypower':[],}
+            self.journalaccount = pd.DataFrame(acct)
             self.buypower = 1e6
 
-    def buy(pcode,pdate,pprice,pquantity):
-        self.buypower
-        self.account = self.account.append({'ts_code':pcode,'date':pdate,'price':pprice,'quantity':pquantity}, ignore_index=True)
-
-    def sell(pcode,pdate,pprice,pquantity):
+    def buysell(self,pbs,pdate):
+        self.buypower = self.buypower - ( pbs['price'] * pbs['Shares'] * pbs['pbuysell'] )
+        self.journalaccount = self.journalaccount.append({'ts_code':pbs['ts_code'],'date':pdate,'price':pbs['price'],'Shares':pbs['Shares'],'buysell':pbs['buysell'],'buypower':self.buypower}, ignore_index=True)
 
 if __name__ == '__main__':
     backtest = Backtest(pstartdate='20190410',penddate='20190412')
